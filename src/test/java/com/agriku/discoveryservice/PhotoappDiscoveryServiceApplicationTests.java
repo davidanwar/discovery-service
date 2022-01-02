ba357ae272db8ea61533e2f9b3c95e05c0ac42c5
david <david@bippo.co.id>
2022-01-02 07:17:03 +0700

    Initial commit

---
+++ src/test/java/com/agriku/discoveryservice/PhotoappDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agriku.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
